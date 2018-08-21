bb79db7b08ca69526644fb59e7d7f645a38f6916
FumiyaShibusawa <shizai.asdfghjkl@gmail.com>
2018-08-21 22:46:21 +0900

    feat: added request method to google dictionary(temp)

--- lib/grammar_checker.rb
+++ lib/grammar_checker.rb
@@ -1,7 +1,17 @@
+require 'net/http'
+
 class GrammarChecker
   class << self
     def strip_method_names_from(file)
       File.read(file).scan(/(def)\s{1}(.+)/).map { |_, name| name }
     end
   end
+end
+
+def request_google_dictionary(query)
+  url = URI.parse("https://www.google.com/search?q=define+#{query}")
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+  res = http.request(Net::HTTP::Get.new(url.path))
+  puts res.body
 end
