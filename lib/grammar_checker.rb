247a1dd1377377655e6346f617165e2373fb5b1d
FumiyaShibusawa <shibusawa@basicinc.jp>
2018-08-23 23:56:45 +0900

    update: added vscode to gitignore

--- lib/grammar_checker.rb
+++ lib/grammar_checker.rb
@@ -5,13 +5,12 @@ class GrammarChecker
     def strip_method_names_from(file)
       File.read(file).scan(/(def)\s{1}(.+)/).map { |_, name| name }
     end
+    def request_google_dictionary(query)
+      url = URI.parse("https://www.google.com/search?q=define+#{query}")
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      res = http.request(Net::HTTP::Get.new(url.path))
+      res.body
+    end
   end
-end
-
-def request_to_google_dictionary(query)
-  url = URI.parse("https://www.google.com/search?q=define+#{query}")
-  http = Net::HTTP.new(url.host, url.port)
-  http.use_ssl = true
-  res = http.request(Net::HTTP::Get.new(url))
-  puts res.body
 end
