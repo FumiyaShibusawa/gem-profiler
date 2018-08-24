8291e574f3d25e6705b294ef50836830f0d1b4a4
FumiyaShibusawa <shibusawa@basicinc.jp>
2018-08-24 21:17:36 +0900

    fix: fixed url.path -> url

--- lib/grammar_checker.rb
+++ lib/grammar_checker.rb
@@ -5,12 +5,13 @@ class GrammarChecker
     def strip_method_names_from(file)
       File.read(file).scan(/(def)\s{1}(.+)/).map { |_, name| name }
     end
-    def request_google_dictionary(query)
+
+    # don't know why search result won't show google dictionary's snippet at the top
+    def request_to_google_dictionary(query)
       url = URI.parse("https://www.google.com/search?q=define+#{query}")
       http = Net::HTTP.new(url.host, url.port)
       http.use_ssl = true
-      res = http.request(Net::HTTP::Get.new(url.path))
-      res.body
+      res = http.request(Net::HTTP::Get.new(url))
     end
   end
 end
