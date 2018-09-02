6cb0fb3988341cd555676e788b16dbf5c8ad574c
FumiyaShibusawa <shizai.asdfghjkl@gmail.com>
2018-09-02 23:48:51 +0900

    update: created parse_method_names method to examine each words with part-of-speech tags

--- lib/grammar_checker.rb
+++ lib/grammar_checker.rb
@@ -1,11 +1,11 @@
-module GrammarChecker
-  def strip_method_names_from(file)
-    File.read(file).scan(/(def)\s{1}(.+)[\s|\(]/).map { |_, name| name.gsub(/_/, "\n") }
-  end
-
-  def parse_method_names(method_names)
-    File.open("sample.txt", "w") { |f| f.write(method_names) }
-    system("treetagger-wrapper/bin/tree-tagger -token treetagger-wrapper/english-par-linux-3.2-utf8.bin sample.txt")
-    File.delete("sample.txt")
+require "tempfile"
+class GrammarChecker
+  def parse_method_names(file)
+    method_names = File.read(file).scan(/(def)\s{1}([^\s|\(]+)/).map { |_, name| name.gsub(/_/, "\n") }.join("\n.\n")
+    Tempfile.open do |t|
+      t.write(method_names)
+      t.open
+      system("treetagger-wrapper/bin/tree-tagger -token treetagger-wrapper/english-par-linux-3.2-utf8.bin #{t.path}")
+    end
   end
 end
